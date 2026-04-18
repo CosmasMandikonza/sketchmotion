7c711865ea3996c1cc72079beca6796df98e2d84
Cosmas Mandikonza <cosmas.t.mandikonza@gmail.com>
2026-04-17 22:32:59 -0500

    Add landing page and auth page with login and signup flow

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,24 @@
+import { GradientBackground } from "@/components/layout/GradientBackground";
+import { Navbar } from "@/components/landing/Navbar";
+import { HeroSection } from "@/components/landing/HeroSection";
+import { FeaturesSection } from "@/components/landing/FeaturesSection";
+import { TestimonialsSection } from "@/components/landing/TestimonialsSection";
+import { PricingSection } from "@/components/landing/PricingSection";
+import { CTASection } from "@/components/landing/CTASection";
+import { Footer } from "@/components/landing/Footer";
+
+export function LandingPage() {
+  return (
+    <GradientBackground>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <PricingSection />
+        <TestimonialsSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </GradientBackground>
+  );
+}
